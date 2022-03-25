59436436d231351a9290053557ba56d1be07b40c
Joshua Smith <jcsmith2393@gmail.com>
2022-03-24 19:54:01 -0500

    Exercise 2 from class 12
    right now it's really messy but i believe everything is nested how it's supposed to be

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { FriendsComponent } from './friends/friends.component';
+import { FriendsListComponent } from './friends-list/friends-list.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PostsComponent,
+    FriendsComponent,
+    FriendsListComponent,
+    MyProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
